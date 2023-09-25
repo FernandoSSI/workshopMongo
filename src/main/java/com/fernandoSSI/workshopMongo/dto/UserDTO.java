5c9fce59d53d5beca4b222ed188068969efe9416
FernandoSSI <fernandosi2005@gmail.com>
2023-09-24 21:18:36 -0300

    create UserDTO

---
+++ src/main/java/com/fernandoSSI/workshopMongo/dto/UserDTO.java
@@ -0,0 +1,45 @@
+package com.fernandoSSI.workshopMongo.dto;
+
+import com.fernandoSSI.workshopMongo.domain.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+
+    UserDTO(){}
+
+    public UserDTO(User obj) {
+        id=obj.getId();
+        name=obj.getName();
+        email=obj.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
