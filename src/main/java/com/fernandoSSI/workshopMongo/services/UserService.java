1f26042b7ad933aae3aae5b6f0aecc9074319843
FernandoSSI <fernandosi2005@gmail.com>
2023-09-26 19:46:06 -0300

    insert User

--- src/main/java/com/fernandoSSI/workshopMongo/services/UserService.java
+++ src/main/java/com/fernandoSSI/workshopMongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.fernandoSSI.workshopMongo.services;
 
 import com.fernandoSSI.workshopMongo.domain.User;
+import com.fernandoSSI.workshopMongo.dto.UserDTO;
 import com.fernandoSSI.workshopMongo.repositories.UserRepository;
 import com.fernandoSSI.workshopMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
