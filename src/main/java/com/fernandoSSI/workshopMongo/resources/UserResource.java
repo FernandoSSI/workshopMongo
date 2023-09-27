f5c26688266ee1b53a482783408a46a60b5e7036
FernandoSSI <fernandosi2005@gmail.com>
2023-09-27 16:10:55 -0300

    endpoint to return the users posts

--- src/main/java/com/fernandoSSI/workshopMongo/resources/UserResource.java
+++ src/main/java/com/fernandoSSI/workshopMongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.fernandoSSI.workshopMongo.resources;
 
+import com.fernandoSSI.workshopMongo.domain.Post;
 import com.fernandoSSI.workshopMongo.domain.User;
 import com.fernandoSSI.workshopMongo.dto.UserDTO;
 import com.fernandoSSI.workshopMongo.services.UserService;
@@ -54,6 +55,12 @@ public class UserResource {
         obj.setId(id);
         obj= service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts" ,method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 
